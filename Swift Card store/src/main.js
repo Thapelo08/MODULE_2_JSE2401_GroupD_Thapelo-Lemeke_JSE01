b0590b7ac230eee097acb5a90f985f436398fcec
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-19 12:59:44 +0200

    "Added Alpine.data 'homeComponent' with products array and fetchProducts function"

--- Swift Card store/src/main.js
+++ Swift Card store/src/main.js
@@ -13,7 +13,21 @@ document.addEventListener("alpine:init", () => {
         },
     }));
 
-    
+    Alpine.data("homeComponent", () => ({
+        products: [],
+        async fetchProducts() {
+            try {
+                const response = await
+                fetch ("https://fakestoreapi.com/products");
+                if (!response.ok) {
+                    throw new error(
+                           "Data fetching failed, please check your network connection");
+                    
+                }
+                
+            }
+        }
+    }))
 })
 
 
